399b9a2790fba85254f3a072e6580c57e0894846
GabrielGQA <gabrielgqa@gmail.com>
2020-01-14 20:03:47 -0300

    evita duplicação de dev

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -4,22 +4,30 @@ const Dev = require('../models/Dev');
 module.exports = {
   async store(req, res) {
     const { git_username, techs, latitude, longitude } = req.body;
-    const response = await axios.get(`https://api.github.com/users/${git_username}`);
-    const { name = login, avatar_url, bio } = response.data;
-    const techsArray = techs.split(',').map(tech => tech.trim());
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude],
-    };
 
-    const dev = await Dev.create({
-      git_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+    let dev = await Dev.findOne({ git_username });
+
+    if(!dev) {
+      const response = await axios.get(`https://api.github.com/users/${git_username}`);
+    
+      const { name = login, avatar_url, bio } = response.data;
+      
+      const techsArray = techs.split(',').map(tech => tech.trim());
+      
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      };
+
+      const dev = await Dev.create({
+        git_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
 
     return res.status(200).json(dev);
   }

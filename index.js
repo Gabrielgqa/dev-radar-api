fc61d1a96341fc147c2ddc869b9a03e10e14843a
GabrielGQA <gabrielgqa@gmail.com>
2020-01-14 18:41:10 -0300

    conexão com o mongo

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://gabriel:devradar@cluster0-yjuqq.mongodb.net/dbradar?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.get('/', (req, res) => {
   return res.json({ message: 'API OK!' });

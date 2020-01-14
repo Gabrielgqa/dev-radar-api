1ef666d7dd36528feaae6136cdd7072931da5e1c
GabrielGQA <gabrielgqa@gmail.com>
2020-01-14 20:01:02 -0300

    transfere regra de negócio para controller

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
@@ -8,27 +7,6 @@ routes.get('/', (req, res) => {
   return res.json({ message: 'API OK!' });
 });
 
-routes.post('/devs', async (req, res) => {
-  const { git_username, techs, latitude, longitude } = req.body;
-  const response = await axios.get(`https://api.github.com/users/${git_username}`);
-  const { name = login, avatar_url, bio } = response.data;
-  const techsArray = techs.split(',').map(tech => tech.trim());
-  const location = {
-    type: 'Point',
-    coordinates: [longitude, latitude],
-  };
-
-  const dev = await Dev.create({
-    git_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArray,
-    location
-  });
-
-  return res.status(200).json(dev);
-
-})
+routes.post('/devs', DevController.store)
 
 module.exports = routes;
